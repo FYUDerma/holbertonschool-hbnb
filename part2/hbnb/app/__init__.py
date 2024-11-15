813d4ab584bd6bf7147ff63cfc7ac171e1494e28
FYUDerma <zakaria.12ans@gmail.com>
2024-11-15 14:58:32 +0100

    Add authentication namespace to the API

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 
+from app.api.v1.auth import api as auth_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -31,6 +32,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     )
 
     # Add namespaces to the API
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
